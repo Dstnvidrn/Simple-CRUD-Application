3daaac7f14002aa77ae8f4262af61d494d7e7790
Kruzik762 <75099373+Kruzik762@users.noreply.github.com>
2022-02-04 21:12:53 -0600

    Assignment13 Completion

---
+++ Assignment13/src/main/java/com/coderscampus/assignment13/service/AccountService.java
@@ -0,0 +1,43 @@
+package com.coderscampus.assignment13.service;
+
+import com.coderscampus.assignment13.domain.Account;
+import com.coderscampus.assignment13.domain.User;
+import com.coderscampus.assignment13.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
+public class AccountService {
+
+
+    @Autowired
+    private AccountRepository accountRepository;
+    @Autowired
+    private UserService userService;
+
+    public Account findByAccountId(Long accountId) {
+        Optional<Account> optAccount = accountRepository.findById(accountId);
+        if (optAccount.isPresent()) {
+            return optAccount.get();
+        } else {
+            throw new RuntimeException("Did not find ID: " + accountId);
+        }
+    }
+
+    public Account saveAccount(Account account) {
+        return accountRepository.save(account);
+    }
+
+
+    public Account createAccount(User user) {
+        Account account = new Account();
+        user.getAccounts().add(account);
+        account.setAccountName("Account #" + user.getAccounts().size());
+        account.getUsers().add(user);
+        userService.saveUser(user);
+        return saveAccount(account);
+    }
+}
---
+++ Assignment13/src/main/java/com/coderscampus/assignment13/service/UserService.java
@@ -0,0 +1,63 @@
+package com.coderscampus.assignment13.service;
+
+import com.coderscampus.assignment13.domain.Account;
+import com.coderscampus.assignment13.domain.User;
+import com.coderscampus.assignment13.repository.AccountRepository;
+import com.coderscampus.assignment13.repository.AddressRepository;
+import com.coderscampus.assignment13.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepo;
+    @Autowired
+    private AccountRepository accountRepo;
+    @Autowired
+    private AddressRepository addressRepository;
+
+    public List<User> findByUsername(String username) {
+        return userRepo.findByUsername(username);
+    }
+
+
+    public User findExactlyOneUserByUsername(String username) {
+        List<User> users = userRepo.findExactlyOneUserByUsername(username);
+        if (users.size() > 0)
+            return users.get(0);
+        else
+            return new User();
+    }
+
+    public User findByUserIdAndAccount(Long userId) {
+        return userRepo.findByUserIdAndAccounts(userId);
+    }
+
+    public Set<User> findAll() {
+        return userRepo.findAllUsersWithAccountsAndAddresses();
+    }
+
+    public User findById(Long userId) {
+        return userRepo.findByUserId(userId);
+    }
+
+    public User saveUser(User user) {
+        if (user.getUserId() == null) {
+            Account newAccount = new Account();
+            newAccount.getUsers().add(user);
+            user.getAccounts().add(newAccount);
+            newAccount.setAccountName("Account #" + user.getAccounts().size());
+            accountRepo.save(newAccount);
+        }
+        return userRepo.save(user);
+    }
+
+    public void delete(Long userId) {
+        userRepo.deleteById(userId);
+    }
+}
